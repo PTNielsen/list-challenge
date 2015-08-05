2a7e4ad4564cc699a6af0a7223e06ad08f6fcf4f
Patrick Nielsen <ptnielsen55@gmail.com>
2015-08-05 15:09:27 -0400

    Adjustments made

--- test.rb
+++ test.rb
@@ -3,19 +3,51 @@ require 'pry'
 
 class ListAnalyzer
 
-  def initialize
+  def starts_with_vowel word
+    word.start_with?("a", "e", "i", "o", "u")
   end
 
-  def number_of_words_starting_with_vowel
-    @produce.each do | item |
-      if item.start_with?("a", "e", "i", "o", "u")
-        return item
+  def number_of_words_starting_with_a_vowel words
+    words_starting_with_vowel = 0
+    words.each do |word|
+      if starts_with_vowel word
+        words_starting_with_vowel += 1
+      end
+    end
+    words_starting_with_vowel
+  end
+
+  def all_words_start_with_vowels? words
+    words.each do |word|
+      unless starts_with_vowel word
+        false
       end
     end
   end
-end
 
-#binding.pry
+  def number_of_vowels_in_all_words words
+    number_of_vowels = 0
+    words.each do |word|
+      letters = word.split("")
+      letters.each do |letter|
+        if starts_with_vowel letter
+          number_of_vowels += 1
+        end
+      end
+    end
+    number_of_vowels
+  end
+
+  def some_word_starts_with_a_vowel? words
+    words.each do |word|
+      if starts_with_vowel word
+        return true
+      end
+    end
+    return false
+  end
+
+end
 
 class ListTest < MiniTest::Test
   def test_it_can_count_vowels
@@ -30,6 +62,7 @@ class ListTest < MiniTest::Test
     mostly_vowels = ["iceberg", "ugli", "endive", "plum", "olive"]
 
     lister = ListAnalyzer.new
+    binding.pry
     refute lister.all_words_start_with_vowels?(mostly_vowels)
 
     mostly_vowels.delete "plum"
